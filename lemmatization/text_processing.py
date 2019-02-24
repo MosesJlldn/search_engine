a144c824d7f9c9b06b597a7eb156c7222d5f4813
MosesZ <mozas97@gmail.com>
2019-02-25 02:57:48 +0300

    lemm

--- lemmatization/text_processing.py
+++ lemmatization/text_processing.py
@@ -8,28 +8,34 @@ from string import punctuation
 import os
 import csv
 import re
+import string
 from collections import Counter
 
 def lemmatize_text(text):
 
-	# tokenizer = RegexpTokenizer(r'\w+')
-	# words = tokenizer.tokenize(text.lower())
-
 	lemmatizer = Mystem() 
-	lemmatized_words = lemmatizer.lemmatize(text) # [lemmatizer.lemmatize(f) for f in filtered_words]
+	lemmatized_words = lemmatizer.lemmatize(text)
 	stop_words = stopwords.words("russian")
-	stop_words.extend(['...', '«', '»', '–'])
+	stop_words.append('…')
 	filtered_words = [w for w in lemmatized_words if w not in stop_words and w != " " and w.strip() not in punctuation]
 
 	return filtered_words
 
-for html_text_file in os.listdir('scraper/webpage_text'):
-	with open('webpage_text/' + html_text_file, 'r') as myfile:
-		text=myfile.read().replace('\n', ' ')
+path = '' #path to html_texts
+
+for html_text_file in os.listdir(path):
+
+	with open(path + '\\' + html_text_file, 'r') as myfile:
+
+		text = myfile.read().replace('\n', ' ')
 		text = re.sub(' +', ' ', text)
+		exclude = string.punctuation + '–«»'
+		text = ''.join(ch for ch in text if ch not in exclude)
 		lemmatized_text = lemmatize_text(text)
 		words_count = Counter(lemmatized_text)
+
 		with open('lemmatized_texts/' + html_text_file + '_lemmatized.csv', 'w', newline='') as f:
+			
 			w = csv.DictWriter(f, words_count.keys())
 			w.writeheader()
 			w.writerow(words_count)
