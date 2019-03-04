5462390d887507cb80dbc5c38f1be22a55011c3c
MosesZ <mozas97@gmail.com>
2019-03-04 16:32:32 +0300

    index

---
+++ scraper/inverted_index.py
@@ -0,0 +1,47 @@
+import os
+import csv
+import re
+
+def sorted_aphanumeric(data):
+
+    convert = lambda text: int(text) if text.isdigit() else text.lower()
+    alphanum_key = lambda key: [ convert(c) for c in re.split('([0-9]+)', key) ] 
+    return sorted(data, key=alphanum_key)
+
+all_docs_dict = []
+
+path = 'C:\\Users\\Moses\\Documents\\GitHub\\search_engine\\lemmatization\\lemmatized_texts' #path to html_texts
+
+words_in_docs = []
+listdir = os.listdir(path)
+sorted_listdir = sorted_aphanumeric(listdir)
+
+for lemm_text in sorted_listdir:
+
+	with open(path + '\\' + lemm_text, 'r') as f:
+
+		reader = csv.reader(f)
+		data = []
+		data = [row for row in reader]
+		words_in_doc = data[0]
+		words_in_docs.append(words_in_doc)
+		not_in_all_dict = [word for word in words_in_doc if word not in all_docs_dict]
+		all_docs_dict.extend(not_in_all_dict)
+
+dict_size = len(all_docs_dict)
+
+words_indicies = []
+
+for i in range(0, 100):
+
+	word_indicies = []
+
+	for word in all_docs_dict:
+
+		if (word in words_in_docs[i]):
+
+			word_indicies.append(i)
+
+	words_indicies.append(word_indicies)
+
+print(words_indicies)
