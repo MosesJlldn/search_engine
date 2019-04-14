a277b516a2dbf9f1ac36af588b362bfeb47b4d8f
MosesZ <mozas97@gmail.com>
2019-04-15 00:46:33 +0300

    update pr

--- page_rank/pr.py
+++ page_rank/pr.py
@@ -13,14 +13,14 @@ def read_links():
 
 def calc_page_rank(page, links_map, N, d=0.75):
 
-	connected_links = [key for key, value in links_map.items() if page in value]
+	connected_links = [key for key, value in links_map.items() if page in value] #links that refer to page
+
 	return connected_links
 
 if __name__=="__main__":
 
-	links_map = read_links()
-	links = list(links_map.keys())
+	links_map = read_links() # key - page, value - links on page
+	links = list(links_map.keys()) # all page links names
 	N = len(links_map)
 	page_rank = [1 / N for i in range(0, 100)]
-	print(links[1])
 	print(calc_page_rank(links[1], links_map, N))
