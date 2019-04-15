3b84f5b858837f6689fd038c1ba6f91e3c12b4d5
MosesZ <mozas97@gmail.com>
2019-04-15 03:16:02 +0300

    Update page_rank.py

--- page_rank/page_rank.py
+++ page_rank/page_rank.py
@@ -20,33 +20,36 @@ def calc_page_rank(page, links_map, page_rank_map, links_on_page_map, N, d=0.75)
 
 	connected_links = [key for key, value in links_map.items() if page in value] #links that refer to page
 
-	sum_list = [(page_rank_map[link] / links_on_page_map[link]) for link in connected_links]
+	sum_list = [(page_rank_map[link] / links_on_page_map[link]) for link in connected_links] #summ(PR / L)
 	res = ((1 - d) / N) + d * sum(sum_list)
 
 	return res
 
 if __name__=="__main__":
 
-	links_map = read_links() # key - page, value - links on page
+	links_map = read_links() # key - page, value - [links on page]
 	links = list(links_map.keys()) # all page links names
 
-	links_amount = [len(value) for key, value in links_map.items()]
-	links_on_page_map = dict(zip(links, links_amount))
+	links_amount = [len(value) for key, value in links_map.items()] # calc amount of links on every page (L)
+	links_on_page_map = dict(zip(links, links_amount)) # map - page: links amount
 
-	N = len(links_map)
-	page_rank = [1 / N for i in range(N)]
-	page_rank_map = dict(zip(links, page_rank))
-	
+	N = len(links_map) # amount of pages in collection
+	page_rank = [1 / N for i in range(N)] # default page rank
+	page_rank_map = dict(zip(links, page_rank)) # map - page: page rank
+
+	#calc page rank 100 times
 	for i in range(0, 100):
 		page_rank = []
 		page_rank = [calc_page_rank(page, links_map, page_rank_map, links_on_page_map, N) for page in links]
-		page_rank_map = dict(zip(links, page_rank))
+		page_rank_map = dict(zip(links, page_rank)) # update current page rank of all pages
 
+	#sort by value
 	sorted_result = sorted(page_rank_map.items(), key=operator.itemgetter(1), reverse=True)
 	k = [res[0] for res in sorted_result]
 	v = [res[1] for res in sorted_result]
 	sorted_result = zip(k, v)
 
+	#write results
 	with open('result.csv', 'w', newline='') as f:
 		writer = csv.writer(f)
 		for row in sorted_result:
